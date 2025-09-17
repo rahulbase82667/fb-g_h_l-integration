d2cf0e8a6111e47cbc267bdd3015bc05ff16090d
rahulbase82667 <rahulbase82667@gmail.com>
2025-09-17 12:47:47 +0530

    added logs dir to the server backend

--- workers/loginWorker.js
+++ workers/loginWorker.js
@@ -7,6 +7,7 @@ import { loginFacebookAccount } from "../services/puppeteerLogin.js";
 const connection = new Redis(process.env.REDIS_URL, {
   maxRetriesPerRequest: null
 });
+
 let io;
 export function setSocketIO(ioInstance) {
   io = ioInstance;
